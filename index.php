d1b5146855516223b144dd5abbda31ddfc5d2895
Max <maxblanck@outlook.fr>
2020-12-09 09:39:47 +0100

    php Partie 1  Exercice 7

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
+<?php
+$lastname = 'Blanck';
+$firstname = 'Max';
+$age = 31;
+?>
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Exercice 7 Partie 1</title>
 </head>
+
 <body>
     <h1>Exercice 7 Partie 1</h1>
-
-    <?php
-    $lastname = "Blanck";
-    $firstname = "Max";
-    $age = 31;
-    echo "Bonjour $lastname $firstname, tu as $age ans.";
-    ?>
-    <?php
-    $lastname = "Blanck";
-    $firstname = "Max";
-    $age = 31;
-    echo 'Bonjour ' . $lastname . ' ' . $firstname . ' tu as ' . $age . ' ans.'; 
-    ?>
-
+    <p><?= 'Bonjour ' . $lastname . ' ' . $firstname . ', tu as ' . $age . ' ans.'; ?></p>
 
 
 </body>
+
 </html>
